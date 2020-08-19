872bf58a5ee167bbf70b2b54078b500f6017e1cc
Kevin Power <kevin.power.97.kp@gmail.com>
2020-08-19 16:40:18 +0100

    hof

---
+++ lab-05-higherOrderFunctions.hs
@@ -0,0 +1,53 @@
+import Data.Char
+
+mytwice :: (a -> a) -> a -> a
+
+mytwice f x
+
+double :: Num a => a -> a
+
+double x
+
+myList :: (a -> b) -> (a -> Bool) -> [a] -> [b]
+
+myList f p xs
+
+myMap :: (a -> b) -> [a] -> [b]
+
+myMap p
+
+myUpper :: (a -> b) -> [a] -> [b]
+
+myUpper p xs
+
+allSquared :: Num a => [a] -> [a]
+
+allSquared xs
+
+nestedreverse :: [String] -> [String]
+
+nestedreverse xs
+
+atfront :: a -> [[a]] -> [[a]]
+
+atfront a xs
+
+myLength :: [String] -> [Int]
+
+myLength xs
+
+sumsq :: Int -> [Int]
+
+sumsq n
+
+vowels :: String
+
+vowels
+
+wvowels :: String -> String
+
+wvowels s
+
+wiv :: [String] -> [String]
+
+wiv xs
