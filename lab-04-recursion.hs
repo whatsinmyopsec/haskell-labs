2eda077eeaebb052ea638177ecf2f79a17aa93bb
Kevin Power <kevin.power.97.kp@gmail.com>
2020-09-01 00:12:48 +0100

    recursion

--- lab-04-recursion.hs
+++ lab-04-recursion.hs
@@ -1,59 +1,82 @@
 sumdown :: Int -> Int
-
-sumdown n
+sumdown 0 = 0
+sumdown n = n + sumdown (n - 1)
 
 exponention :: Int -> Int -> Int
-
-exponention
+exponention _ 0 = 1
+exponention n e = exponention n (e - 1)
 
 fibonacci :: Int -> Int
-
-fibonacci
-
-mySum :: Num a => [a] -> a
-
-mySum
-
-myProduct :: Num a => [a] -> a
-
-myProduct
-
-doubleAll :: Num a => [a] -> [a]
-
-doubleAll
+fibonacci 0 = 0
+fibonacci 1 = 1
+fibonacci i = fibonacci (i - 1) + fibonacci (i - 2)
 
 myInit :: [a] -> [a]
-
-myInit
+myInit [_] = []
+myInit (x : xs) = x : myInit xs
 
 myAnd :: [Bool] -> Bool
-
-myAnd
+myAnd [] = True
+myAnd (x : xs) = x == myAnd xs
 
 myConcat :: [[a]] -> [a]
-
-myConcat
+myConcat [] = []
+myConcat (x : xs) = x ++ myConcat xs
 
 myReplicate :: Int -> a -> [a]
-
-myReplicate
-
-insert :: Ord a => a -> [a] -> [a]
-
-insert
-
-insertSort :: Ord a => [a] -> [a]
-
-inserSort
+myReplicate 0 _ = []
+myReplicate i a = myReplicate (i - 1) a
 
 myNth :: Ord a => [a] -> Int -> a
-
-myNth
+myNth (x : xs) 0 = x
+myNth (x : xs) n = myNth (xs) (n -1)
 
 myElm :: Ord a => [a] -> a -> Bool
+myElm [] _ = False
+myElm (x : xs) a = a == x || myElm xs a
 
-myElm
+mySum :: Num a => [a] -> a
+mySum [] = 0
+mySum (i : is) = i + mySum is
 
-myDrop :: [a] -> Int -> [a]
+myTake :: Int -> [b] -> [b]
+myTake 0 _ = []
+myTake _ [] = []
+myTake i (x : xs) = x : myTake (i - 1) xs
 
-myDrop
+myLast :: [a] -> a
+myLast [x] = x
+myLast (_ : xs) = myLast xs
+
+myProduct :: Num a => [a] -> a
+myProduct [] = 1
+myProduct (i : is) = i * myProduct is
+
+doubleAll :: Num a => [a] -> [a]
+doubleAll [] = []
+doubleAll (i : is) = i * 2 : doubleAll is
+
+isort :: Ord a => a -> [a] -> [a]
+isort x [] = [x]
+isort x (y : ys)
+  | x <= y = x : y : ys
+  | otherwise = y : isort x ys
+
+insertSort :: Ord a => [a] -> [a]
+insertSort [] = []
+insertSort (x : xs) = isort x (insertSort xs)
+
+mapLookup :: [(Integer, String)] -> [Integer] -> [String]
+mapLookup _ [] = []
+mapLookup ns (i : is) =
+  locate i : mapLookup ns is
+  where
+    locate i =
+      let go i ns =
+            case ns of
+              [] -> undefined
+              ((i', n) : xs) ->
+                if i == i'
+                  then n
+                  else go i xs
+       in go i ns
